d506f35753c140ad7997011345a96d66edc9f377
ebis19 <elu1996biscaia@gmail.com>
2018-10-12 15:41:40 -0300

    Escenario

--- src/snake/Escenario.java
+++ src/snake/Escenario.java
@@ -1,5 +1,32 @@
 package snake;
 
+import java.util.ArrayList;
+
 public class Escenario {
+	
+	int id;
+	int h,l;
+	ArrayList<ObjetoEnElPlano> elementos;
+	int [][] plano;
+	
+	public void  agregarElemento(ObjetoEnElPlano obj){
+		elementos.add(obj);
+		plano[obj.x][obj.y]=elementos.size()-1;
+	}
+	
+
+	public void  moverElemento(int x, int y , int x2,int y2){
+		
+	}
+	public void  quitar(int x, int y){
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
