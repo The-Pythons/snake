3e73316d3b667a89b38b982c04ee599209d4157a
MauroC97 <42740061+MauroC97@users.noreply.github.com>
2018-10-19 15:28:22 -0300

    clase muy basica de puntos2d

---
+++ src/snake/Punto2D.java
@@ -0,0 +1,18 @@
+package snake;
+
+public class Punto2D {
+	int x;
+	int y;
+	
+	public Punto2D() {
+		this.x=0;
+		this.y=0;
+	}
+	
+	public Punto2D(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}
