aa8d8124ea6558656055f12bf9ad2928e8d923a3
ebis19 <elu1996biscaia@gmail.com>
2018-11-09 16:37:15 -0300

    Bug fix, comienzo de implementacion del usuario

--- src/logica/Usuario.java
+++ src/logica/Usuario.java
@@ -1,5 +1,21 @@
 package logica;
 
+import snake.Orientacion;
+
 public class Usuario {
+
+	public Orientacion dir=Orientacion.N;
+
+	public Orientacion getDir() {
+		return dir;
+	}
+
+	public void setDir(Orientacion dir) {
+		this.dir = dir;
+	}
+
+	
+	
+
 
 }
